12ae750e97a959d81a5e98e049cc844bfa5c8413
Alexandre de Souza <alexandre@aledsz.com.br>
2024-05-20 20:02:54 -0300

    Apply review comment

--- lib/kino_proxy/application.ex
+++ lib/kino_proxy/application.ex
@@ -4,8 +4,7 @@ defmodule KinoProxy.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {PartitionSupervisor,
-       child_spec: KinoProxy.Client.child_spec([]), name: KinoProxy.PartitionSupervisor}
+      {PartitionSupervisor, child_spec: KinoProxy.Client, name: KinoProxy.PartitionSupervisor}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: KinoProxy.Supervisor)
